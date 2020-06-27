9d523d349c1de7cb02e84687933249c8a945694d
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-06-27 16:50:30 +0300

    Added bishop moves support.

--- lib/chess/ChessRules.php
+++ lib/chess/ChessRules.php
@@ -40,9 +40,38 @@ class ChessRules
     }
 
 
+    public static function CoordToLetter(int $coord): string 
+    {
+        switch ($coord) 
+        {
+            case 0: return 'a';
+            case 1: return 'b';
+            case 2: return 'c';
+            case 3: return 'd';
+            case 4: return 'e';
+            case 5: return 'f';
+            case 6: return 'g';
+            case 7: return 'h';
+            default: return '';
+        }
+    }
+
+
     public static function MoveNotationToCoords(string $move): array 
     {
         return [intval($move[1]) - 1, ChessRules::LetterToCoord($move[0])];
+    }
+
+
+    public static function CoordsToMoveNotation(array $coords): string 
+    {
+        return ChessRules::CoordToLetter($coords[1]).($coords[0] + 1);
+    }
+
+
+    public static function InBoard(int $row, int $col): bool 
+    {
+        return ($row >= 0) && ($row <= 7) && ($col >= 0) && ($col <= 7);
     }
 
 
@@ -55,7 +84,7 @@ class ChessRules
     public static function IsEmpty(string $letter):  bool { return $letter === '.'; }
 
 
-    public static function GetPawnMoves(ChessBoard $board, int $row, int $col) 
+    public static function GetPawnMoves(ChessBoard $board, int $row, int $col): array
     {
         $result = [];
 
@@ -78,12 +107,65 @@ class ChessRules
     }
 
 
+    public static function GetBishopMoves(ChessBoard $board, int $row, int $col): array 
+    {
+        $result = [];
+
+        // Left-top
+        $checkRow = $row + 1;
+        $checkCol = $col - 1;
+        while (ChessRules::InBoard($checkRow, $checkCol) && ChessRules::IsEmpty($board->GetAt($checkRow, $checkCol))) 
+        {
+            $result[] = [$checkRow, $checkCol];
+            ++$checkRow;
+            --$checkCol;
+        }
+
+        // Right-top
+        $checkRow = $row + 1;
+        $checkCol = $col + 1;
+        while (ChessRules::InBoard($checkRow, $checkCol) && ChessRules::IsEmpty($board->GetAt($checkRow, $checkCol)))
+        {
+            $result[] = [$checkRow, $checkCol];
+            ++$checkRow;
+            ++$checkCol;
+        }
+
+        // Right-bot
+        $checkRow = $row - 1;
+        $checkCol = $col + 1;
+        while (ChessRules::InBoard($checkRow, $checkCol) && ChessRules::IsEmpty($board->GetAt($checkRow, $checkCol)))
+        {
+            $result[] = [$checkRow, $checkCol];
+            --$checkRow;
+            ++$checkCol;
+        }
+
+        // Left-bot
+        $checkRow = $row - 1;
+        $checkCol = $col - 1;
+        while (ChessRules::InBoard($checkRow, $checkCol) && ChessRules::IsEmpty($board->GetAt($checkRow, $checkCol)))
+        {
+            $result[] = [$checkRow, $checkCol];
+            --$checkRow;
+            --$checkCol;
+        }
+
+        return $result;
+    }
+
+
     // Move in 'e2-e4'
     public static function IsCorrectMove(ChessBoard $board, string $move): bool
     {
-        $coords = explode('-', $move);
-        
-        if (ChessRules::IsPawn($board->GetAt(... ChessRules::MoveNotationToCoords($coords[0]))))
-            return in_array(ChessRules::MoveNotationToCoords($coords[1]), ChessRules::GetPawnMoves($board, ... ChessRules::MoveNotationToCoords($coords[0])));
+        list($from, $to) = explode('-', $move);
+
+        $piece = $board->GetAt(... ChessRules::MoveNotationToCoords($from));
+        if (ChessRules::IsPawn($piece))
+            return in_array(ChessRules::MoveNotationToCoords($to), ChessRules::GetPawnMoves($board, ... ChessRules::MoveNotationToCoords($from)));
+        if (ChessRules::IsBishop($piece))
+            return in_array(ChessRules::MoveNotationToCoords($to), ChessRules::GetBishopMoves($board, ... ChessRules::MoveNotationToCoords($from)));
+
+        return false;
     }
 }
