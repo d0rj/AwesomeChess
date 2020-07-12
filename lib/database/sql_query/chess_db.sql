1e546029c587392f630867b8992744242ca08272
d0rj_ <dima.balobin_barnaul@mail.ru>
2020-07-12 10:25:10 +0300

    Added sql query to create database for application.

---
+++ lib/database/sql_query/chess_db.sql
@@ -0,0 +1,17 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Версия сервера: 10.3.22-MariaDB
+-- Версия PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- База данных: `chess_db`
+--
+CREATE DATABASE IF NOT EXISTS `chess_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `chess_db`;
